021284733e3e72596e58995641231ecc1019c61a
parks <parksungho86@gmail.com>
2017-11-30 16:05:23 +0900

    update

--- app/pages/log/daily/daily.js
+++ app/pages/log/daily/daily.js
@@ -12,7 +12,7 @@
         var params = {};
 
         $scope.detail = function () {
-            return $rootScope.user && $rootScope.user.permissions.contains("DAILY_LOG_DETAIL");
+            return $rootScope.user && $rootScope.user.permissions && $rootScope.user.permissions.contains("DAILY_LOG_DETAIL");
         };
 
         $scope.more = function () {
