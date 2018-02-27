6213401c0422ccccc91ddd727c399772386e3887
parks <parksungho86@gmail.com>
2018-02-27 19:00:45 +0900

    update

--- app/pages/log/dailyPoint/daily.js
+++ app/pages/log/dailyPoint/daily.js
@@ -36,7 +36,8 @@ var list = [];
             new CashEvent("ADMIN_POINT", "운영자 지급/차감"),
             new CashEvent("SIGNUP_POINT", "회원가입 축하 지급"),
             new CashEvent("OPEN_LOCAL_TODAYCARD", "지역별 투데이 카드 더보기"),
-            new CashEvent("OPEN_CUSTOM_TODAY", "맞춤 소개 받기")
+            new CashEvent("OPEN_CUSTOM_TODAY", "맞춤 소개 받기"),
+            new CashEvent("EVALUATE_BONUS", "평가 보너스", 1)
         ];
 
         function Type(fn, name) {
